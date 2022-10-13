7987b360dcd05a28bf3a940966d73d8beaf31b2a
sachin <desai.sachin45@gmail.com>
2022-10-13 11:49:59 +0530

    wip

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import Cam from "./Cam";
 
 const App = () => {
+  const [recordStart, setRecordStart] = useState(false);
   return (
     <div>
-      <Cam />
+      <h1>Recording & Live Preview</h1>
+      <button
+        onClick={() => {
+          setRecordStart(true);
+        }}
+      >
+        Start setup
+      </button>
+      <button
+        onClick={() => {
+          setRecordStart(false);
+        }}
+      >
+        Stop setup
+      </button>
+      <div>{recordStart && <Cam />}</div>
     </div>
   );
 };
--- src/Cam.js
+++ src/Cam.js
@@ -1,92 +1,72 @@
-import { useCallback, useMemo, useRef, useState } from "react";
-import Webcam from "react-webcam";
+import React from "react";
+import { useReactMediaRecorder } from "react-media-recorder";
+
+function LiveStreamPreview({ stream }) {
+  let videoPreviewRef = React.useRef();
+
+  React.useEffect(() => {
+    if (videoPreviewRef.current && stream) {
+      videoPreviewRef.current.srcObject = stream;
+    }
+  }, [stream]);
+
+  if (!stream) {
+    console.log("No stream available");
+    return null;
+  }
+
+  return <video ref={videoPreviewRef} width={520} height={480} autoPlay />;
+}
 
 const Cam = () => {
-  const webcamRef = useRef(null);
-  const mediaRecorderRef = useRef(null);
-  const [capturing, setCapturing] = useState(false);
-  const [recordedChunks, setRecordedChunks] = useState([]);
+  const onRecordStop = (blobURL, blob) => {
+    console.log({ blob });
+    var fileOfBlob = new File([blob], `Recorded-${Math.random() * 10}-version`);
+    console.log({ fileOfBlob });
+    //startRecording();
+  };
 
-  const constraints = useMemo(() => {
-    return {
-      audio: {
-        channelCount: 1,
-        sampleRate: 16000,
-        sampleSize: 16,
-        volume: 1,
-      },
-      video: {
-        height: 720,
-        width: 1280,
-        facingMode: "user",
-      },
-    };
-  }, []);
+  const {
+    status,
+    startRecording,
+    stopRecording,
+    mediaBlobUrl,
+    //clearBlobUrl,
+    previewStream,
+  } = useReactMediaRecorder({
+    onStop: onRecordStop,
+    video: true,
+    askPermissionOnMount: true,
+    blobOptions: { type: "video/webm" },
+    mediaStreamConstraints: { audio: true, video: true },
+  });
 
-  const handleDataAvailable = useCallback(
-    ({ data }) => {
-      if (data.size > 0) {
-        setRecordedChunks((prev) => prev.concat(data));
-      }
-    },
-    [setRecordedChunks]
-  );
+  console.log({ previewStream });
 
-  const handleStartCaptureClick = useCallback(() => {
-    setCapturing(true);
-    mediaRecorderRef.current = new MediaRecorder(webcamRef.current.stream, {
-      mimeType: "video/webm",
-    });
-    mediaRecorderRef.current.addEventListener(
-      "dataavailable",
-      handleDataAvailable
-    );
-    mediaRecorderRef.current.start();
-  }, [webcamRef, setCapturing, mediaRecorderRef, handleDataAvailable]);
-
-  const handleStopCaptureClick = useCallback(() => {
-    mediaRecorderRef.current.stop();
-    setCapturing(false);
-  }, [mediaRecorderRef, setCapturing]);
-
-  const handleDownload = useCallback(() => {
-    if (recordedChunks.length) {
-      const blob = new Blob(recordedChunks, {
-        type: "video/webm",
-      });
-      const url = URL.createObjectURL(blob);
-      const a = document.createElement("a");
-      document.body.appendChild(a);
-      a.style = "display: none";
-      a.href = url;
-      a.download = "react-webcam-stream-capture.webm";
-      a.click();
-      window.URL.revokeObjectURL(url);
-      setRecordedChunks([]);
-    }
-  }, [recordedChunks]);
+  const stopCurrentRecording = () => {
+    stopRecording();
+  };
   return (
-    <>
-      <div className="w-[320px] mx-auto relative">
-        <Webcam audio={true} ref={webcamRef} videoConstraints={constraints} />
+    <div>
+      <h5>Status: {status}</h5>
+      <div>
+        <button onClick={startRecording} disabled={status === "recording"}>
+          Start Recording
+        </button>
+        <button
+          onClick={stopCurrentRecording}
+          disabled={status !== "recording"}
+        >
+          Stop Recording
+        </button>
       </div>
-      <div className="fixed bottom-0 left-0 w-full p-2 bg-black/80 flex items-center justify-center gap-3">
-        {capturing ? (
-          <button onClick={handleStopCaptureClick} className="btn">
-            Stop Capture
-          </button>
-        ) : (
-          <button onClick={handleStartCaptureClick} className="btn">
-            Start Capture
-          </button>
-        )}
-        {recordedChunks.length > 0 && (
-          <button onClick={handleDownload} className="btn">
-            Download
-          </button>
-        )}
+      <h3>Live:</h3>
+      <div>{previewStream && <LiveStreamPreview stream={previewStream} />}</div>
+      <h3>Recorded:</h3>
+      <div>
+        {mediaBlobUrl && <video src={mediaBlobUrl} controls autoPlay loop />}
       </div>
-    </>
+    </div>
   );
 };
 
